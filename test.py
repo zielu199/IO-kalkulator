9227de4138658206b26dd05654840671231d4ebf
Kacper Zielinski <68391@student.ue.poznan.pl>
2022-03-07 17:56:31 +0100

    Dodano kalkulator z dodawaniem

--- test.py
+++ test.py
@@ -3,4 +3,11 @@
 def hello(name):
 	return "Hello" + str(name)
 
-print("Hello world")
+def dodaj (a,b):
+	wynik = float(a)+float(b)
+	return wynik
+
+pierwsza = input()
+druga = input()
+
+print (dodaj(pierwsza, druga))
